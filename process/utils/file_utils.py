107582c6cd5a3bd3cbaadec18854697c48f93bac
Mehmet-Bener <142571454+Memuaw@users.noreply.github.com>
2025-01-16 22:19:55 +0300

    Delete process directory

--- process/utils/file_utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def cleanup_audio_files():
-    """Deletes temporary audio files to free up space."""
-    for file in ["output.mp3", "output.wav"]:
-        if os.path.exists(file):
-            os.remove(file)
-            print(f"Deleted temporary file: {file}")
